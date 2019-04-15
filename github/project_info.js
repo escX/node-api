a859e505649a6173fa5532e58e28d49a678fb420
escX <1768810189@qq.com>
2019-04-15 23:10:03 +0800

    add toUpperCase & change filename

--- github/project_info.js
+++ github/project_info.js
@@ -1,8 +1,9 @@
 const get_project_info = require('../spider/get_project_info');
 
 const project_info = ({query, source, method}) => {
-  if (method === 'GET') {
-    return get_project_info(source);
+  switch(method.toUpperCase()) {
+    case 'GET':
+      return get_project_info(source);
   }
 }
 
