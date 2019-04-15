6c963fb3c92febc65ad2153cef7a69bac25dc15f
escX <1768810189@qq.com>
2019-04-16 01:24:32 +0800

    handle cros

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const http = require('http');
 const handle_request = require('./handle_request');
 
 http.createServer((request, response) => {
+  const header = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS'
+  };
+
   handle_request(request).then(info => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.writeHead(200, header);
     response.end(JSON.stringify(info));
   }).catch(({code, error}) => {
-    response.writeHead(code, {'Content-Type': 'application/json'});
+    response.writeHead(code, header);
     response.end(JSON.stringify({
       error
     }));
